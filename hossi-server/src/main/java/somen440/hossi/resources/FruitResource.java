d7a63b6b8c615b737945cbc2007c012820661154
satoya <mentol310@gmail.com>
2021-03-25 20:52:34 +0900

    req/res を別の型として持つように

--- hossi-server/src/main/java/somen440/hossi/resources/FruitResource.java
+++ hossi-server/src/main/java/somen440/hossi/resources/FruitResource.java
@@ -1,22 +1,18 @@
 package somen440.hossi.resources;
 
 import org.jboss.logging.Logger;
-import somen440.hossi.usecases.fruits.FruitData;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
 import somen440.hossi.usecases.fruits.add.FruitAddInputData;
-import somen440.hossi.usecases.fruits.add.FruitAddOutputData;
 import somen440.hossi.usecases.fruits.add.FruitAddUseCase;
 import somen440.hossi.usecases.fruits.delete.FruitDeleteInputData;
-import somen440.hossi.usecases.fruits.delete.FruitDeleteOutputData;
 import somen440.hossi.usecases.fruits.delete.FruitDeleteUseCase;
 import somen440.hossi.usecases.fruits.list.FruitListInputData;
-import somen440.hossi.usecases.fruits.list.FruitListOutputData;
 import somen440.hossi.usecases.fruits.list.FruitListUseCase;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import java.util.Collections;
-import java.util.LinkedHashMap;
+import java.util.HashSet;
 import java.util.Set;
 
 @Path("/fruits")
@@ -35,21 +31,85 @@ public class FruitResource {
     @Inject
     FruitDeleteUseCase fruitDeleteUseCase;
 
+    public static class Fruit {
+
+        public int id;
+        public String name;
+        public String description;
+
+        public Fruit() {}
+
+        public Fruit(int id, String name, String description) {
+            this.id = id;
+            this.name = name;
+            this.description = description;
+        }
+    }
+
+    public static class FruitListRequest {}
+
+    public static class FruitListResponse {
+
+        public Set<Fruit> fruits;
+
+        public FruitListResponse() {}
+
+        public FruitListResponse(Set<Fruit> fruits) {
+            this.fruits = fruits;
+        }
+    }
+
     @GET
-    public FruitListOutputData list(FruitListInputData input) {
-        LOG.info("list");
-        return this.fruitListUseCase.handle(input);
+    public FruitListResponse list(FruitListRequest req) {
+        final var input = new FruitListInputData();
+        final var fruits = this.fruitListUseCase.handle(input).fruits.stream().map(
+                fruitData -> new Fruit(fruitData.id, fruitData.name, fruitData.description)
+        ).toList();
+
+        LOG.info(String.format("list size=%d", fruits.size()));
+
+        return new FruitListResponse(new HashSet<>(fruits));
+    }
+
+    public static class FruitAddRequest {
+
+        public String name;
+        public String description;
+
+        public FruitAddRequest() {}
+
+        public FruitAddRequest(String name, String description) {
+            this.name = name;
+            this.description = description;
+        }
+    }
+
+    public static class FruitAddResponse {
+
+        public Fruit fruit;
+
+        public FruitAddResponse() {}
+
+        public FruitAddResponse(Fruit fruit) {
+            this.fruit = fruit;
+        }
     }
 
     @POST
-    public FruitAddOutputData add(FruitAddInputData input) {
-        LOG.info("add");
-        return this.fruitAddUseCase.handle(input);
+    public FruitAddResponse add(FruitAddRequest req) {
+        final var input = new FruitAddInputData(req.name, req.description);
+        final var output = this.fruitAddUseCase.handle(input);
+
+        LOG.info(String.format("add id=%d", output.fruit.id));
+
+        return new FruitAddResponse(new Fruit(output.fruit.id, output.fruit.name, output.fruit.description));
     }
 
     @DELETE
-    public FruitDeleteOutputData delete(FruitDeleteInputData input) {
-        LOG.info("delete");
-        return this.fruitDeleteUseCase.handle(input);
+    @Path("/{id}")
+    public void delete(@PathParam int id) {
+        this.fruitDeleteUseCase.handle(new FruitDeleteInputData(id));
+
+        LOG.info(String.format("delete id=%d", id));
     }
 }
