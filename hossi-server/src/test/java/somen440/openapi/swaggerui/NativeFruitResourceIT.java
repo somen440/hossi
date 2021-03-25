ae875c2b5becd521d1702cc0ddc3cf942c6e7825
satoya <mentol310@gmail.com>
2021-03-25 12:32:22 +0900

    add: template

---
+++ hossi-server/src/test/java/somen440/openapi/swaggerui/NativeFruitResourceIT.java
@@ -0,0 +1,9 @@
+package somen440.openapi.swaggerui;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeFruitResourceIT extends FruitResourceTest {
+
+    // Execute the same tests but in native mode.
+}
