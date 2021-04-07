c56e7ccec51e7b7764e90c96b9316059f0384dc4
satoya <mentol310@gmail.com>
2021-04-07 10:41:58 +0900

    RuntimeException を自作型に変えてみる

---
+++ hossi-server/src/main/java/org/somen440/hossi/exception/RuntimeException.java
@@ -0,0 +1,11 @@
+package org.somen440.hossi.exception;
+
+public class RuntimeException extends Exception {
+  public RuntimeException() {
+    super();
+  }
+
+  public RuntimeException(String message) {
+    super(message);
+  }
+}
