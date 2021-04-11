d6007c1ddb5b2572b1178492caf1aea2f9c0bc0c
satoya <mentol310@gmail.com>
2021-04-11 12:15:52 +0900

    add: prettier

--- hossi-front/.eslintrc.js
+++ hossi-front/.eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
   ],
   rules: {
   },
--- hossi-front/server/main.ts
+++ hossi-front/server/main.ts
@@ -1,15 +1,15 @@
-import express, { Request, Response } from 'express';
-import register from '@react-ssr/express/register';
-import { config, toMaskConfig } from './config/config';
+import express, { Request, Response } from "express";
+import register from "@react-ssr/express/register";
+import { config, toMaskConfig } from "./config/config";
 
 const app = express();
 
 (async () => {
   await register(app);
 
-  app.get('/', (_req: Request, res: Response) => {
-    const user = { name: 'World' };
-    res.render('index', { user });
+  app.get("/", (_req: Request, res: Response) => {
+    const user = { name: "World" };
+    res.render("index", { user });
   });
 
   app.listen(config.port, () => {
