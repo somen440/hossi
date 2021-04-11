1c6bede673d65d708986cc68b8e31ee10278cd3c
satoya <mentol310@gmail.com>
2021-04-11 10:42:12 +0900

    add: template

---
+++ hossi-front/server/config.ts
@@ -0,0 +1,3 @@
+const config = {
+  PORT: process.env.PORT || 3000,
+}
---
+++ hossi-front/server/main.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import register from '@react-ssr/express/register';
+
+const app = express();
+
+(async () => {
+  await register(app);
+
+  app.get('/', (_req: Request, res: Response) => {
+    const user = { name: 'World' };
+    res.render('index', { user });
+  });
+
+  app.listen(3000, () => {
+    console.log('> Ready on http://localhost:3000');
+  });
+})();
